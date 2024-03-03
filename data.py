ae75fd41e7913374c785a08a95546b52903f2568
Aimme Vivas Ruiz <152124515+Aimmevivas@users.noreply.github.com>
2024-03-03 20:49:48 -0300

    data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-urban_routes_url = ' https://cnt-96b2901d-513a-4a3e-95d0-7c9be7d29a2f.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-232afc9d-b259-4f97-b3e1-55cb050a2072.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
 card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
+message_for_driver = 'Traeme los snacks'
